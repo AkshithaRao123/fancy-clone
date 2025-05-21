40c85f34f10e992135741a0347139fcb0a1c3713
AkshithaRao123 <akshashw@gmail.com>
2025-05-21 13:56:40 +0530

    awards added, marquee animated

---
+++ src/assets/awards/award_imports.jsx
@@ -0,0 +1,15 @@
+import awtest from './awtest.svg';
+import clutch from './Clutch.svg';
+import css_design from './CSSDesignAwards.svg'
+import producthunt from './Product_hunt.svg';
+import sortlist from './Sortlist.svg';
+import trustpilot from './Trustpilol.svg';
+
+export {
+    awtest,
+    clutch,
+    css_design,
+    producthunt,
+    sortlist,
+    trustpilot
+}
--- src/components/home_page/Banner.jsx
+++ src/components/home_page/Banner.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import { awtest, clutch, css_design, producthunt, sortlist, trustpilot } from '../../assets/awards/award_imports'
 
 const Banner = () => {
     return (
-        <div className='w-3/8 absolute left-5/8 flex flex-col flex-wrap justify-center items-center h-screen'>
+        <div className='w-4/9 absolute left-5/9 flex flex-col flex-wrap justify-center items-center h-screen'>
             <div className='bg-black p-10'>
                 <h1 className='font-semibold text-7xl mb-5'>Design Subscription for Startups</h1>
                 <h3 className='text-lg font-semibold'>Your on-call design team that can deliver branding, websites, apps, and more!</h3>
+            </div>
+            <div className="bg-white w-full flex flex-row justify-center items-center overflow-hidden">
+                <div className='flex flex-row justify-center items-center h-[5rem] marquee overflow-hidden'>
+                    <div className='border border-r-gray-300'><img src={awtest} /></div>
+                    <div className='border border-r-gray-300'><img src={clutch} /></div>
+                    <div className='border border-r-gray-300'><img src={css_design} /></div>
+                    <div className='border border-r-gray-300'><img src={producthunt} /></div>
+                    <div className='border border-r-gray-300'><img src={sortlist} /></div>
+                    <div className='border border-r-gray-300'><img src={trustpilot} /></div>
+                </div>
             </div>
         </div>
     )
 }
 
 export default Banner
+
+// marquee animation-(--marquee)
