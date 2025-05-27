e896da6a94b29a9b105b179e3c443a9b71d3f85d
AkshithaRao123 <akshashw@gmail.com>
2025-05-27 14:31:29 +0530

    cleaned brogress bar layout

--- src/components/home_page/ProcessDescription.jsx
+++ src/components/home_page/ProcessDescription.jsx
@@ -11,18 +11,18 @@ const ProcessDescription = () => {
 
     return (
         <div className="w-screen px-20 sticky h-[90vh] top-30 flex flex-row">
-            <div>
+            <div className="w-2/3">
                 <div id="process" className="border-l-4 pl-15">
                     <div className="pb-10">
-                        <h1 className="font-bold text-6xl">Start Subscription</h1>
+                        <h1 className="font-bold text-5xl">Start Subscription</h1>
                         <p className="font-semibold text-2xl py-5">Just book a call</p>
                     </div>
                     <div className="pb-10">
-                        <h1 className="font-bold text-6xl">Describe the task</h1>
+                        <h1 className="font-bold text-5xl">Describe the task</h1>
                         <p className="font-semibold text-2xl py-5">Tell us what you need</p>
                     </div>
                     <div>
-                        <h1 className="font-bold text-6xl">Get Designs</h1>
+                        <h1 className="font-bold text-5xl">Get Designs</h1>
                         <p className="font-semibold text-2xl pt-5">Start receiving drafts in 48 hrs</p>
                     </div>
                 </div>
